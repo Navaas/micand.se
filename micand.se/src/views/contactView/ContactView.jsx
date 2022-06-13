c66b8e9d0750f22c0c32c28cc0358cfbf64a9ada
micand89 <89338483+micand89@users.noreply.github.com>
2022-06-13 20:38:12 +0200

    update

--- micand.se/src/views/contactView/ContactView.jsx
+++ micand.se/src/views/contactView/ContactView.jsx
@@ -20,8 +20,8 @@ const ContactView = () => {
                                 <img className={css.image} src={mail} alt="logo"/>
                             </div>
                             <div className={css.flipCardBack}>
-                                <h2>Contact me:</h2>
-                                <p>mi-and@outlook.com</p>
+                                <p className={css.text}>Send me a mail</p>
+                                <p className={css.text}>mi-and@outlook.com</p>
                             </div>
                         </div>
                     </div>
@@ -33,9 +33,9 @@ const ContactView = () => {
                                 <img className={css.image} src={linkedIn} alt="github"/>
                             </div>
                             <div className={css.flipCardBack}>
-                                <span>Check out my LinkedIn and send me a invite</span>
+                                <p className={css.text}>Check out my LinkedIn and send me a invite</p>
                                 <form action="https://github.com/micand89/Portfolie" method="get" target="_blank">
-                                    <button type="submit" className={css.button}>Click me</button>
+                                    <button type="submit" className={css.button}>View</button>
                                 </form>
                             </div>
                         </div>
@@ -48,10 +48,10 @@ const ContactView = () => {
                                 <img className={css.image} src={github} alt="github"/>
                             </div>
                             <div className={css.flipCardBack}>
-                                <span>Check out some of my project on github</span>
+                                <p className={css.text}>Check out some of my project on github</p>
                                 <form action="https://www.linkedin.com/in/michaela-andreasson-421824224/" method="get"
                                       target="_blank">
-                                    <button type="submit" className={css.button}>Click me</button>
+                                    <button type="submit" className={css.button}>View</button>
                                 </form>
                             </div>
                         </div>
