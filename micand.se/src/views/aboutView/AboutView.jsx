6b773eeb096f29ecc8ba574c6d3a9b8bd357d4f6
micand89 <89338483+micand89@users.noreply.github.com>
2022-06-13 12:22:09 +0200

    update

--- micand.se/src/views/aboutView/AboutView.jsx
+++ micand.se/src/views/aboutView/AboutView.jsx
@@ -1,5 +1,5 @@
 import css from './AboutView.module.css'
-import githubIcon from '../../utils/images/github.png'
+import profileImage from '../../utils/images/micand.png'
 
 const AboutView = () => {
     return (
@@ -7,18 +7,20 @@ const AboutView = () => {
             <div className={css.container}>
                 <section className={css.empty}></section>
                 <section className={css.containerImage}>
-                    <img src={githubIcon} alt="github" className={css.image}/>
+                    <img src={profileImage} alt="profile" className={css.profileImage}/>
                 </section>
-                <section className={css.containerText}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Accusamus
-                    dignissimos eligendi error
-                    impedit odit pariatur perspiciatis, vitae. Accusamus architecto aspernatur dignissimos ea enim nam
-                    quas
-                    quia quod rerum ut? Consequatur facere ipsam ut. Adipisci aliquid consequatur, explicabo incidunt
-                    iste
-                    maiores molestias nemo non porro quaerat quibusdam suscipit temporibus ullam veniam.
+                <section className={css.containerText}>
+                    My name is Michaela and i live in Stenungsund with my family just north of Gothenburg.
                     <br/>
-                    <button className={css.buttonCv}>Till mitt cv</button>
+                    <br/>
+                    I study to become a fullstack developer, which will give me a good understanding about both frontend and backend. So far i'm more interested in backend.
+                    The role as a developer gives me the opportunity to be a problemsolver aswell as bringing out the creative side of me.
+                    <br/>
+                    <br/>
+                    As a person i like to keep things in order which is good because i try to write my code as clean as possible.
+                    I work structured, i like to try new things and i have a keen eye for design.
+                    <br/>
+                    <button className={css.buttonCv}>Mitt cv</button>
                 </section>
                 <section className={css.empty}></section>
             </div>
--- micand.se/src/views/contactView/ContactView.jsx
+++ micand.se/src/views/contactView/ContactView.jsx
@@ -1,20 +1,58 @@
 import css from './ContactView.module.css'
+import mail from '../../utils/images/mail.png'
+import linkedIn from '../../utils/images/linkedin1.png'
+import github from '../../utils/images/github.png'
+
 
 const ContactView = () => {
     return (
         <>
             <div className={css.container}>
-                <section className={css.empty}></section>
-                <section className={css.containerText}>
-                    <p>Mail: mi-and@outlook.com</p>
+                <section>
                 </section>
-                <section className={css.containerText}>
-                    <p>LinkedIn</p>
+                <section className={css.gridContainer}>
+                    <div className={css.flipCard}>
+                        <div className={css.flipCardInner}>
+                            <div className={css.flipCardFront}>
+                                <img className={css.image} src={mail} alt="logo"/>
+                            </div>
+                            <div className={css.flipCardBack}>
+                                <h2>Contact me:</h2>
+                                <p>mi-and@outlook.com</p>
+                            </div>
+                        </div>
+                    </div>
                 </section>
-                <section className={css.containerText}>
-                    <p>GitHub</p>
+                <section className={css.gridContainer}>
+                    <div className={css.flipCard}>
+                        <div className={css.flipCardInner}>
+                            <div className={css.flipCardFront}>
+                                <img className={css.image} src={linkedIn} alt="github"/>
+                            </div>
+                            <div className={css.flipCardBack}>
+                                <span>Check out my LinkedIn and send me a invite</span>
+                                <form action="https://github.com/micand89/Portfolie" method="get" target="_blank">
+                                    <button type="submit" className={css.button}>Click me</button></form>
+                            </div>
+                        </div>
+                    </div>
                 </section>
-                <section className={css.empty}></section>
+                <section className={css.gridContainer}>
+                    <div className={css.flipCard}>
+                        <div className={css.flipCardInner}>
+                            <div className={css.flipCardFront}>
+                                <img className={css.image} src={github} alt="github"/>
+                            </div>
+                            <div className={css.flipCardBack}>
+                                <span>Check out some of my project on github</span>
+                                <form action="https://www.linkedin.com/in/michaela-andreasson-421824224/" method="get" target="_blank">
+                                    <button type="submit" className={css.button}>Click me</button></form>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                </section>
             </div>
         </>
     )
