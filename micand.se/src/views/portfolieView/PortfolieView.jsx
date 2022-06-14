df6408ce7fae335253a1dcf9369aa06392b43082
micand89 <89338483+micand89@users.noreply.github.com>
2022-06-14 10:59:35 +0200

    update

--- micand.se/src/views/portfolieView/PortfolieView.jsx
+++ micand.se/src/views/portfolieView/PortfolieView.jsx
@@ -12,24 +12,24 @@ const PortfolieView = () => {
             </section>
             <div className={css.container}>
                 <section>
-                    <h1>TodoList</h1>
-                    <h3>React, JavaScript</h3>
+                    <h1 className={css.text}>TodoList</h1>
+                    <h3 className={css.smallText}>React, JavaScript</h3>
                     <img src={image1} alt="image1" className={css.image}/>
                     <br/>
                     <br/>
                     <button className={css.button}>View</button>
                 </section>
                 <section className={ `${css.sectionBox} ${css.sectionBorder}`} >
-                    <h1>SmurfBook</h1>
-                    <h3>React, TypeScript</h3>
+                    <h1 className={css.text}>SmurfBook</h1>
+                    <h3 className={css.smallText}>React, TypeScript</h3>
                     <img src={image2} alt="image2" className={css.image}/>
                     <br/>
                     <br/>
                     <button className={css.button}>View</button>
                 </section>
                 <section>
-                    <h1>Webbshop</h1>
-                    <h3>React, JavaScript</h3>
+                    <h1 className={css.text}>Webbshop</h1>
+                    <h3 className={css.smallText}>React, JavaScript</h3>
                     <img src={image3} alt="image3" className={css.image}/>
                     <br/>
                     <br/>
