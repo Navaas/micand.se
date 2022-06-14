d99d64bb28acd2e2a1cd58696a90be20695baf05
micand89 <89338483+micand89@users.noreply.github.com>
2022-06-14 10:16:50 +0200

    update

--- micand.se/src/views/portfolieView/PortfolieView.jsx
+++ micand.se/src/views/portfolieView/PortfolieView.jsx
@@ -6,32 +6,37 @@ import image3 from '../../utils/images/wait.png'
 
 const PortfolieView = () => {
     return (
-        <div className={css.container}>
+        <>
             <section>
-                <h1>TodoList</h1>
-                <h3>React, JavaScript</h3>
-                <img src={image1} alt="image1" className={css.image}/>
-                <br/>
-                <br/>
-                <button className={css.button}>View</button>
+                <h1 className={css.textContact}>Portfolie</h1>
             </section>
-            <section className={css.border}>
-                <h1>SmurfBook</h1>
-                <h3>React, TypeScript</h3>
-                <img src={image2} alt="image2" className={css.image}/>
-                <br/>
-                <br/>
-                <button className={css.button}>View</button>
-            </section>
-            <section>
-                <h1>Webbshop</h1>
-                <h3>React, JavaScript</h3>
-                <img src={image3} alt="image3" className={css.image}/>
-                <br/>
-                <br/>
-                <button className={css.button}>View</button>
-            </section>
-        </div>
+            <div className={css.container}>
+                <section>
+                    <h1>TodoList</h1>
+                    <h3>React, JavaScript</h3>
+                    <img src={image1} alt="image1" className={css.image}/>
+                    <br/>
+                    <br/>
+                    <button className={css.button}>View</button>
+                </section>
+                <section className={css.sectionBox}>
+                    <h1>SmurfBook</h1>
+                    <h3>React, TypeScript</h3>
+                    <img src={image2} alt="image2" className={css.image}/>
+                    <br/>
+                    <br/>
+                    <button className={css.button}>View</button>
+                </section>
+                <section>
+                    <h1>Webbshop</h1>
+                    <h3>React, JavaScript</h3>
+                    <img src={image3} alt="image3" className={css.image}/>
+                    <br/>
+                    <br/>
+                    <button className={css.button}>View</button>
+                </section>
+            </div>
+        </>
     )
 }
 
