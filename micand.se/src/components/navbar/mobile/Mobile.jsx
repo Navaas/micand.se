8935bf4d93fa8e1a7f6e6cf728c086a73729a7b6
micand89 <89338483+micand89@users.noreply.github.com>
2022-08-03 16:21:52 +0200

    updaterat hamburger menu

--- micand.se/src/components/navbar/mobile/Mobile.jsx
+++ micand.se/src/components/navbar/mobile/Mobile.jsx
@@ -1,11 +1,14 @@
 import Menu from './menu/Menu'
 import {useRef, useState} from "react";
 import Hamburger from './hamburger/Hamburger'
+import { useOnClickOutside } from "../../../utils/hooks/useOnClickOutside";
 
 const Mobile = () => {
     const [showMenu, setShowMenu] = useState(false)
     const ref = useRef(null)
     const closeMenu = () => setShowMenu(false)
+
+    useOnClickOutside(ref, closeMenu)
 
     const toggleMenu = () => {
         setShowMenu(!showMenu)
